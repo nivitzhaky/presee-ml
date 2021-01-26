b74dc97cdde4a7cf880c521cadd2099722b79ba7
Niv Itzhaky <niv.itzhaky@gmail.com>
2021-01-26 19:06:18 +0200

    v1

--- app.py
+++ app.py
@@ -5,32 +5,34 @@ from flask import Flask, render_template, session, redirect, url_for, request
 import csv
 import os.path
 warnings.filterwarnings("ignore")
-from keras.models import load_model
-from keras.optimizers import Adam
+# from keras.models import load_model
+# from keras.optimizers import Adam
+# import tensorflow as tf
 import h5py
 import pandas as pd
-import tensorflow as tf
+# import tensorflow as tf
 
 app = Flask(__name__)
-my_path = os.path.abspath(os.path.dirname(__file__))
-path = os.path.join(my_path, "model.hdf5")
+# my_path = os.path.abspath(os.path.dirname(__file__))
+# path = os.path.join(my_path, "model.hdf5")
 
-model = load_model(path, compile=False)
-model.compile(loss='binary_crossentropy', optimizer=Adam())
-global graph
-graph = tf.get_default_graph()
+# model = load_model(path, compile=False)
+# model.compile(loss='binary_crossentropy', optimizer=Adam())
+# global graph
+# graph = tf.get_default_graph()
 
 @app.route("/",  methods=['POST'])
 def index():
-    f = request.files['file']
-    df = pd.read_csv(f, sep="\t")
-    cur = np.delete(df.values, 12, axis=1)
-    cur = np.divide(cur, 1000.0)
-    cur = cur[0:4096]
-    cur = np.swapaxes(cur,0,1)
-    cur=np.dstack((cur))
-    with graph.as_default():
-        y_score = model.predict(cur, batch_size=10, verbose=1)
-    return str(y_score)
+    # f = request.files['file']
+    # df = pd.read_csv(f, sep="\t")
+    # cur = np.delete(df.values, 12, axis=1)
+    # cur = np.divide(cur, 1000.0)
+    # cur = cur[0:4096]
+    # cur = np.swapaxes(cur,0,1)
+    # cur=np.dstack((cur))
+    # with graph.as_default():
+    #     y_score = model.predict(cur, batch_size=10, verbose=1)
+    # return str(y_score)
+    return ""
 
 app.run(host='0.0.0.0', port=8080)
